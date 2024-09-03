5bcd1d5a6e7b0fada5331d20edca8663acf2454e
Mohd Babar Malik <142683471+BBR6464@users.noreply.github.com>
2024-09-03 16:19:29 +0530

    Add files via upload

---
+++ Project_1_Band_name_generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band name Generator")
+city = input("What is the of city you grow up in? \n")
+pet = input("What is your pet name? \n")
+print("You'r Band name is" + " " +city + " " + pet)
