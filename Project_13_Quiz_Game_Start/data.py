1333cf15f1deba9180707c68e1a83170f7866a5f
Mohd Babar Malik <142683471+BBR6464@users.noreply.github.com>
2024-09-04 08:59:39 +0530

    Add files via upload

---
+++ Project_13_Quiz_Game_Start/data.py
@@ -0,0 +1,92 @@
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "&quot;27 Club&quot; is a term used to refer to a list of famous actors, musicians, "
+                    "and artists who died at the age of 27.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "It is automatically considered entrapment in the United States if the police sell you illegal substances without revealing themselves.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+     },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "Gumbo is a stew that originated in Louisiana.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "In 2010, Twitter and the United States Library of Congress partnered together to archive every tweet by American citizens.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "Pluto is a planet.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "&quot;Ananas&quot; is mostly used as the word for Pineapple in other languages.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "On average, at least 1 person is killed by a drunk driver in the United States every hour.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "Nutella is produced by the German company Ferrero.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "French is an official language in Canada.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+
+]
